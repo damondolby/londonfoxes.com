55444c8a92af406190b330707198e026ce1a57e9
Damon <damondolby@gmail.com>
2016-11-16 22:33:51 +0000

    loading fixtures from json structure

---
+++ data/fixtures.js
@@ -0,0 +1,34 @@
+var fixtures  =  [   
+    {
+	"team": "Watford",
+	"competition": "Premier League",
+	"date": "19 Nov 2016",
+	"time": "15:00"},
+      
+    {
+	"team": "Sunderland",
+	"competition": "Premier League",
+	"date": "03 Dec 2016",
+	"time": "15:00"},
+	{
+	"team": "FC Porto",
+	"competition": "Champions League",
+	"date": "07 Dec 2016",
+	"time": "19:45"},
+	{
+	"team": "AFC Bournemouth",
+	"competition": "Premier League",
+	"date": "13 Dec 2016",
+	"time": "19:45"},
+	{
+	"team": "Stoke City",
+	"competition": "Premier League",
+	"date": "17 Dec 2016",
+	"time": "15:00"},
+	{
+	"team": "Middlesbrough",
+	"competition": "Premier League",
+	"date": "02 Jan 2017",
+	"time": "12:30"}]
+	
+	

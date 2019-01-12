8d3061c5930022ebc557bf41fd25d3f65c0f2217
Damon <damondolby@gmail.com>
2019-01-12 14:49:42 +0000

    update fixtures

--- data/fixtures.js
+++ data/fixtures.js
@@ -1,34 +1,31 @@
 var fixtures  =  [   
-    {
+{
+	"team": "Wolverhampton Wanderers",
+	"competition": "Premier League",
+	"date": "19 Jan 2019",
+	"time": "12:30"},
+{
+	"team": "Liverpool",
+	"competition": "Premier League",
+	"date": "30 Jan 2019",
+	"time": "20:00"},
+{
+	"team": "Tottenham Hotspur",
+	"competition": "Premier League",
+	"date": "10 Feb 2019",
+	"time": "13:30"},
+{
 	"team": "Watford",
 	"competition": "Premier League",
-	"date": "19 Nov 2016",
+	"date": "02 Mar 2019",
 	"time": "15:00"},
-      
-    {
-	"team": "Sunderland",
+{
+	"team": "Burnley",
 	"competition": "Premier League",
-	"date": "03 Dec 2016",
-	"time": "15:00"},
-	{
-	"team": "FC Porto",
-	"competition": "Champions League",
-	"date": "07 Dec 2016",
-	"time": "19:45"},
-	{
-	"team": "AFC Bournemouth",
-	"competition": "Premier League",
-	"date": "13 Dec 2016",
-	"time": "19:45"},
-	{
-	"team": "Stoke City",
-	"competition": "Premier League",
-	"date": "17 Dec 2016",
-	"time": "15:00"},
-	{
-	"team": "Middlesbrough",
-	"competition": "Premier League",
-	"date": "02 Jan 2017",
-	"time": "12:30"}]
+	"date": "16 Mar 2019",
+	"time": "15:00"}
+
+
+]
 	
 	

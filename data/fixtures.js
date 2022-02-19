9a59f0a09bf1ef023db268eac01a8b679f801ad8
Damon <damondolby@gmail.com>
2022-02-19 14:38:18 +0000

    update to latest version

--- data/fixtures.js
+++ data/fixtures.js
@@ -2,27 +2,37 @@ var fixtures  =  [
 {
 	"team": "Wolverhampton Wanderers",
 	"competition": "Premier League",
-	"date": "19 Jan 2019",
-	"time": "12:30"},
+	"date": "20 Feb 2022",
+	"time": "16:30"},
 {
-	"team": "Liverpool",
-	"competition": "Premier League",
-	"date": "30 Jan 2019",
-	"time": "20:00"},
-{
-	"team": "Tottenham Hotspur",
-	"competition": "Premier League",
-	"date": "10 Feb 2019",
-	"time": "13:30"},
-{
-	"team": "Watford",
-	"competition": "Premier League",
-	"date": "02 Mar 2019",
-	"time": "15:00"},
+	"team": "Randers",
+	"competition": "Europa Conference",
+	"date": "24 Feb 2022",
+	"time": "17:45"},
 {
 	"team": "Burnley",
 	"competition": "Premier League",
-	"date": "16 Mar 2019",
+	"date": "1 Mar 2022",
+	"time": "19:45"},
+{
+	"team": "Arsenal",
+	"competition": "Premier League",
+	"date": "13 Mar 2022",
+	"time": "16:30"},
+{
+	"team": "Manchester United",
+	"competition": "Premier League",
+	"date": "2 Apr 2022",
+	"time": "15:00"},
+{
+	"team": "Newcastle United",
+	"competition": "Premier League",
+	"date": "16 Apr 2022",
+	"time": "15:00"},
+{
+	"team": "Tottenham Hotspur",
+	"competition": "Premier League",
+	"date": "30 Apr 2022",
 	"time": "15:00"}
 
 
